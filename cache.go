b2dcd7c1851166e362e6fad8525bd4e5f213c884
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-21 05:04:14 +0100

    feat: add concurrency control to Cache struct and methods

--- cache.go
+++ cache.go
@@ -1,7 +1,10 @@
 package cache
 
+import "sync"
+
 // Cache is key-value pair storage.
 type Cache[K comparable, V any] struct {
+	mu   sync.RWMutex
 	data map[K]V
 }
 
@@ -14,12 +17,16 @@ func New[K comparable, V any]() Cache[K, V] {
 
 // Read returns the associated value for a key, and a boolean true if the key is absent.
 func (c *Cache[K, V]) Read(key K) (V, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	v, found := c.data[key]
 	return v, found
 }
 
 // Upsert overrides the value for a given key.
 func (c *Cache[K, V]) Upsert(key K, value V) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.data[key] = value
 
 	// Don't return an error for the moment, possibly in the future
@@ -28,5 +35,7 @@ func (c *Cache[K, V]) Upsert(key K, value V) error {
 
 // Delete removes the entry for the given key.
 func (c *Cache[K, V]) Delete(key K) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	delete(c.data, key)
 }

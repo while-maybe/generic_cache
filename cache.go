f0e2417323e57f47ea55fa85997c9011fe122d9d
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-22 00:32:03 +0100

    feat: refactor Cache struct to include maxSize and improve Upsert logic

--- cache.go
+++ cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"slices"
 	"sync"
 	"time"
 )
@@ -11,6 +12,9 @@ type Cache[K comparable, V any] struct {
 
 	mu   sync.Mutex
 	data map[K]entryWithTimeout[V]
+
+	maxSize           int
+	chronologicalKeys []K
 }
 
 type entryWithTimeout[V any] struct {
@@ -19,14 +23,16 @@ type entryWithTimeout[V any] struct {
 }
 
 // New creates a usable cache.
-func New[K comparable, V any](ttl time.Duration) Cache[K, V] {
+func New[K comparable, V any](maxSize int, ttl time.Duration) Cache[K, V] {
 	return Cache[K, V]{
-		ttl:  ttl,
-		data: make(map[K]entryWithTimeout[V]),
+		ttl:               ttl,
+		data:              make(map[K]entryWithTimeout[V]),
+		maxSize:           maxSize,
+		chronologicalKeys: make([]K, 0, maxSize),
 	}
 }
 
-// Read returns the associated value for a key, and a boolean true if the key is absent.
+// Read returns the associated value for a key, and a boolean true if the key is present.
 func (c *Cache[K, V]) Read(key K) (V, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -39,8 +45,7 @@ func (c *Cache[K, V]) Read(key K) (V, bool) {
 	case !ok:
 		return zeroV, false
 	case v.expires.Before(time.Now()):
-		// this value is expired
-		delete(c.data, key)
+		c.deleteKeyValue(key)
 		return zeroV, false
 	default:
 		return v.value, ok
@@ -51,10 +56,17 @@ func (c *Cache[K, V]) Read(key K) (V, bool) {
 func (c *Cache[K, V]) Upsert(key K, value V) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.data[key] = entryWithTimeout[V]{
-		value:   value,
-		expires: time.Now().Add(c.ttl),
+
+	_, alreadyExists := c.data[key]
+
+	switch {
+	case alreadyExists:
+		c.deleteKeyValue(key)
+	case len(c.data) == c.maxSize:
+		c.deleteKeyValue(c.chronologicalKeys[0])
 	}
+
+	c.addKeyValue(key, value)
 
 	// Don't return an error for the moment, possibly in the future
 	return nil
@@ -64,5 +76,23 @@ func (c *Cache[K, V]) Upsert(key K, value V) error {
 func (c *Cache[K, V]) Delete(key K) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
+	c.deleteKeyValue(key)
+}
+
+// addKeyValue inserts a key-value pair into the cache.
+func (c *Cache[K, V]) addKeyValue(key K, value V) {
+	c.data[key] = entryWithTimeout[V]{
+		value:   value,
+		expires: time.Now().Add(c.ttl),
+	}
+	c.chronologicalKeys = append(c.chronologicalKeys, key)
+}
+
+// deleteKeyValue removes a key-pair from the cache
+func (c *Cache[K, V]) deleteKeyValue(key K) {
+	c.chronologicalKeys = slices.DeleteFunc(c.chronologicalKeys, func(k K) bool {
+		return k == key
+	})
 	delete(c.data, key)
 }

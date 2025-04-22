1c33bbdd0a7998006a1f1c4d349eceb602f4f3dd
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-23 00:41:25 +0100

    refactor: add remaining test steps

--- cache_internal_test.go
+++ cache_internal_test.go
@@ -16,7 +16,7 @@ func TestCache(t *testing.T) {
 	if c.chronologicalKeys == nil {
 		t.Error("List of keys not initialised")
 	}
-	
+
 	c.Upsert(1, 10)
 	c.Upsert(2, 20)
 	c.Upsert(3, 30)
@@ -33,7 +33,7 @@ func TestCache(t *testing.T) {
 	// A different way of doing the for loop above with the iter pattern for a change
 	dataKeys := make([]int, 0, len(c.data))
 	keyIter := maps.Keys(c.data)
-	keyIter(func (k int) bool {
+	keyIter(func(k int) bool {
 		dataKeys = append(dataKeys, k)
 		return true
 	})
@@ -47,5 +47,41 @@ func TestCache(t *testing.T) {
 	expectedKeys = []int{1, 3, 2}
 	if !slices.Equal(c.chronologicalKeys, expectedKeys) {
 		t.Errorf("After upserting: Keys of the map should be %v, got %v", expectedKeys, c.chronologicalKeys)
+	}
+
+	c.Delete(3)
+
+	// see comment above
+	dataKeys = make([]int, 0, len(c.data))
+	keyIter = maps.Keys(c.data)
+	keyIter(func(k int) bool {
+		dataKeys = append(dataKeys, k)
+		return true
+	})
+
+	slices.Sort(dataKeys)
+	expectedKeys = []int{1, 2}
+	if !slices.Equal(dataKeys, expectedKeys) {
+		t.Errorf("After deleting: Keys of the map should be %v, got %v", expectedKeys, dataKeys)
+	}
+
+	value, found := c.Read(1)
+	if !found {
+		t.Error("Key 1 should be present")
+	}
+
+	if value != 10 {
+		t.Errorf("Value of Key 1 should be 10, got %v", value)
+	}
+
+	// test TTL
+	time.Sleep(time.Second)
+	value, found = c.Read(1)
+	if found {
+		t.Error("Key 1 should have expired but still exists")
+	}
+
+	if value != 0 {
+		t.Errorf("Value of Key 1 should be 0, got %v", value)
 	}
 }
